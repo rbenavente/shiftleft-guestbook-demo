59b5f99f9eccc578e6c70029167a4870503aeb94
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-18 19:08:49 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -1,3 +1,4 @@
+
 PO=$(kubectl get po -n guestbook|grep frontend| awk '{ print $1 }')
 
  echo "Generate legit traffic"
@@ -11,9 +12,9 @@ kubectl run attacker  --image=rbenavente/evilpetclinic -n guestbook
 P1=$(kubectl get po -n guestbook|grep attacker| awk '{ print $1 }')
 
 echo "scan ports"
-kubectl exec -it $P1 -n guestbook -- bash -c "nmap -p 80,8080,8081,8082,8083,8084,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
-kubectl exec -it $P1 -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
-kubectl exec -it $P1 -n guestbook -- bash -c "watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
+kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
+kubectl exec -it $P1 -n guestbook -- sh -c "watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
+kubectl exec -it $P1 -n guestbook -- sh -c "watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 
 command || true
 
