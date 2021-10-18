ffe3cd549292441224c1026f5bcb58b7284ec7bb
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-18 20:34:50 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -15,7 +15,7 @@ kubectl run attacker  --image=rbenavente/evilpetclinic -n guestbook
 P1=$(kubectl get po -n guestbook|grep attacker| awk '{ print $1 }')
 
 echo "scan ports"
-kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379, 2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
+kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
 
 echo "gen non authorized traffic"
 kubectl exec -it $P1 -n guestbook -- sh -c " curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
