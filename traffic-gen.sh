96a15c306d1f1af4d32a4a207a6a6e37d60a809a
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-18 21:30:59 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -5,25 +5,25 @@ kubectl run attacker  --image=rbenavente/evilpetclinic -n guestbook
 
 PO=$(kubectl get po -n guestbook|grep frontend| awk '{ print $1 }')
 P1=$(kubectl get po -n guestbook|grep attacker| awk '{ print $1 }')
-
+if [[ $PO == *"No resources found"* ] or [ $P1 == *"No resources found"* ]]; then
+   sleep 120
+else
 echo "scan ports"
 kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
 
 echo "Generate legit traffic"
+for i in {1..20}; do 
 kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
-kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
+kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get";  
+done
 
 echo "Generate non authorized traffic"
 kubectl exec -i $P1 -n guestbook -- sh -c " curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,,,"
 kubectl exec $P1 -n guestbook -- sh -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 kubectl exec -i $P1 -n guestbook -- sh -c " curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,,,,"
 
-
-command || true
-
-
-
+fi
 
 
 //watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,
