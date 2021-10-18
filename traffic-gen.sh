5f1b941dcd341a8aa9286ad8b68d5e24833b21b0
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-18 20:58:21 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -5,7 +5,7 @@ echo "Generate legit traffic"
 kubectl cp infra/$P:/var/jenkins_home/workspace/shiftleft-guestbook-demo/entrypoint.sh  entrypoint.sh  -c $PO -n guestbook
 kubectl exec -it $PO -n guestbook -- bash -c "./entrypoint.sh"
 
-//kubectl exec -it $PO -n guestbook -- bash -c "watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
+kubectl exec -i $PO -n guestbook -- bash -c "watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 
 
 
@@ -18,9 +18,9 @@ echo "scan ports"
 kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
 
 echo "gen non authorized traffic"
-kubectl exec -it $P1 -n guestbook -- sh -c " curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
+kubectl exec -i $P1 -n guestbook -- sh -c " curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
 kubectl cp infra/P:/var/jenkins_home/workspace/shiftleft-guestbook-demo/entrypoint.sh  entrypoint.sh  -c $P1 -n guestbook
-// kubectl exec -it $P1 -n guestbook -- sh -c "watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
+kubectl exec -i $P1 -n guestbook -- sh -c "watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 
 command || true
 
