88e49b85d649a26b236e56c0f61711ce2eeef822
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-18 21:17:34 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -1,26 +1,23 @@
-PO=$(kubectl get po -n guestbook|grep frontend| awk '{ print $1 }')
-
-echo "Generate legit traffic"
-kubectl cp entrypoint.sh  guestbook/$P0:/entrypoint.sh   
-kubectl exec -it $PO -n guestbook -- bash -c "entrypoint.sh"
-
-kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
-kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
-
-
-
 echo "deploy attacker"
+
 kubectl run attacker  --image=rbenavente/evilpetclinic -n guestbook
 
+PO=$(kubectl get po -n guestbook|grep frontend| awk '{ print $1 }')
 P1=$(kubectl get po -n guestbook|grep attacker| awk '{ print $1 }')
 
 echo "scan ports"
 kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
 
-echo "gen non authorized traffic"
-kubectl exec -i $P1 -n guestbook -- sh -c " curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
-kubectl cp infra/P:/var/jenkins_home/workspace/shiftleft-guestbook-demo/entrypoint.sh  entrypoint.sh  -c $P1 -n guestbook
+echo "Generate legit traffic"
+kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
+kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
+kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
+
+echo "Generate non authorized traffic"
+kubectl exec -i $P1 -n guestbook -- sh -c " curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,,,"
 kubectl exec $P1 -n guestbook -- sh -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
+kubectl exec -i $P1 -n guestbook -- sh -c " curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,,,,"
+
 
 command || true
 
