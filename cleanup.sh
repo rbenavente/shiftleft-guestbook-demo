be02c22a1960188a4ec73d04b3faeffee032815a
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-18 13:46:18 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,4 +4,5 @@ kubectl delete deployment -l app=redis  -n guestbook && \
 kubectl delete service -l app=redis  -n guestbook && \
 kubectl delete deployment frontend  -n guestbook && \
 kubectl delete service frontend  -n guestbook && \
+kubectl delete deployment attacker  -n guestbook && \
 kubectl delete namespace guestbook
