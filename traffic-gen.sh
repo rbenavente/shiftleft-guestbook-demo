67d350de533ea89434c36dfe51bd7a777554b0aa
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-18 21:36:03 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -5,6 +5,7 @@ kubectl run attacker  --image=rbenavente/evilpetclinic -n guestbook
 
 PO=$(kubectl get po -n guestbook|grep frontend| awk '{ print $1 }')
 P1=$(kubectl get po -n guestbook|grep attacker| awk '{ print $1 }')
+
 if [[ $PO == *"No resources found"* ] or [ $P1 == *"No resources found"* ]]; then
    sleep 120
 else
@@ -12,7 +13,7 @@ echo "scan ports"
 kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
 
 echo "Generate legit traffic"
-for i in {1..20}; do 
+for i in {1..10}; do 
 kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
 kubectl exec $PO -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get";  
