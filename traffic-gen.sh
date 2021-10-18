a5fde27eb28091f0091a4fc888e60faacfacc74f
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-18 17:55:55 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -12,6 +12,7 @@ P1=$(kubectl get po -n guestbook|grep attacker| awk '{ print $1 }')
 
 echo "scan ports"
 kubectl exec -it $P1 -n guestbook -- bash -c "nmap -p 80,8080,8081,8082,8083,8084,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
+kubectl exec -it $P1 -n guestbook -- bash -c "curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,"
 kubectl exec -it $P1 -n guestbook -- bash -c "watch curl frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 
 command || true
