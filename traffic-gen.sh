bcf20a1938cb60a0b674edf1994641f71745f512
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-10-19 16:12:28 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -1,7 +1,7 @@
 
 echo "deploy attacker"
 
-kubectl run attacker  --image=rbenavente/gb-guestbook-cns:latest -n guestbook
+kubectl run attacker  --image=rbenavente/gb-frontend-cns:latest -n guestbook
 
 PO=$(kubectl get po -n guestbook|grep frontend| awk '{ print $1 }')
 P1=$(kubectl get po -n guestbook|grep attacker| awk '{ print $1 }')
@@ -9,22 +9,23 @@ P1=$(kubectl get po -n guestbook|grep attacker| awk '{ print $1 }')
 if [[ $PO == *"No resources found"* ] or [ $P1 == *"No resources found"* ]]; then
    sleep 120
 else
-echo "scan ports"
-kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
+
 
 echo "Generate legit traffic"
-// For custom images 
+// For custom images the entrypoint.sh automatically generate http request but we need to enable apache2 server on the container
 kubectl exec $PO -n guestbook -- bash  -c "service apache2 start"
 
-// For default images frm grc.io registry 
+// For default images frm grc.io registry:
 # for i in {1..5}; do
 # kubectl exec $PO -n guestbook -- bash -c "curl -m 2  frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 # kubectl exec $PO -n guestbook -- bash -c "curl -m 2  frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,";  
 # done
 
 echo "Generate non authorized traffic"
-// For custom images 
-kubectl exec $P1 -n guestbook -- bash  -c ""service apache2 start""
+// For custom images: entrypoint.sh automatically generate http request
+
+echo "scan ports"
+kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
 
 // For default images frm grc.io registry 
 # kubectl exec -i $P1 -n guestbook -- sh -c "curl -m 1 frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,,,"
