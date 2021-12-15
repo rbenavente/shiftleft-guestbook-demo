4dae44d72babbc04d98c7ddbbce07d6a36b808ac
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-12-15 21:17:08 +0100

    Update reject-traffic-guestbook.sh

--- reject-traffic-guestbook.sh
+++ reject-traffic-guestbook.sh
@@ -1,5 +1,6 @@
 echo "Reject traffic by default on K8s namespace"
 
 PO=$(./apoctl api list namespace --recursive -f name==/${env.tenant}/${env.cloudAccount}/${env.group}/${env.ns} -c ID -o yaml | awk '{ print $3 }')
+apoctl auht verify
 ./apoctl api update namespace $P0 --namespace /${env.tenant}/${env.cloudAccount}/${env.group} -k  'defaultPUIncomingTrafficAction=Reject' 
 ./apoctl api update namespace $P0 --namespace /${env.tenant}/${env.cloudAccount}/${env.group} -k  'defaultPUOutgoingTrafficAction=Reject' 
