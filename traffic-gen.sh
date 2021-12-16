95b9fa725683c80ea123c7b737e9f3388b8e2e70
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-12-16 11:09:49 +0100

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -12,22 +12,22 @@ else
 
 
 echo "Generate legit traffic"
-// For your own image xxxx/gb-guestbook-cns the entrypoint.sh automatically generate http request but we need to enable apache2 server on the container
+# // For your own image xxxx/gb-guestbook-cns the entrypoint.sh automatically generate http request but we need to enable apache2 server on the container
 kubectl exec $PO -n guestbook -- bash  -c "service apache2 start"
 
-// For default images frm grc.io registry:
+# // For default images frm grc.io registry:
 # for i in {1..5}; do
 # kubectl exec $PO -n guestbook -- bash -c "curl -m 2  frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 # kubectl exec $PO -n guestbook -- bash -c "curl -m 2  frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,";  
 # done
 
 echo "Generate non authorized traffic"
-// For the custom image user/gb-frontend-cns:v1: the entrypoint.sh automatically generate http request
+#  // For the custom image user/gb-frontend-cns:v1: the entrypoint.sh automatically generate http request
 
 echo "scan ports"
 kubectl exec -it $P1 -n guestbook -- sh -c "nmap -p 80,8080,8081,8082,8083,8084,6379,2375,10250,6443,9998 frontend.guestbook.svc.cluster.local"
 
-// For default images from grc.io registry gcr.io/google_samples/gb-frontend:v5: 
+# // For default images from grc.io registry gcr.io/google_samples/gb-frontend:v5: 
 # kubectl exec -i $P1 -n guestbook -- sh -c "curl -m 1 frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,,,"
 # kubectl exec $P1 -n guestbook -- sh -c "curl -m 1 frontend.guestbook.svc.cluster.local/guestbook.php?cmd=get"
 # kubectl exec -i $P1 -n guestbook -- sh -c "curl -m 1 frontend.guestbook.svc.cluster.local/guestbook.php?cmd=set&value=,,,,"
